098052481ab2b486f9b8d3a27bd018e2adc1705f
aldrinbgama <aldrin.gama@umuzi.org>
2020-02-22 17:12:52 +0200

    third commit

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,49 +1,63 @@
-import java.util.List;
-import java.util.Scanner;
 import java.util.Arrays;
-import java.util.stream.IntStream;
-
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
-    static int add(String string) {
-        String delimiter = ",|\n";
 
-        String[] arr = string.split("\n", 2);
-        boolean startsWith = string.startsWith("//");
-
-        if (string == null || string.isEmpty())
-            return 0;
-        if (startsWith) {
-            delimiter = arr[0].substring(2);
-            string = arr[1];
+    public static int add(String input) {
+        StringBuilder deli = new StringBuilder();
+        if (input.startsWith("//")) {
+            deli.append("\n");
+            Matcher m = Pattern.compile("(?<=//).*(?=\n)").matcher(input); //Finding delimiters between // and \n  then pass everything after \n
+            while (m.find()) {
+                deli.append("[").append(m.group()).append("]");
+            }
+            input = input.substring(input.indexOf("\n"));
+        } else {
+            deli.append(",\n");
         }
+        return add(input, "[" + deli + "]");
+    }
 
-        List<String> num = Arrays.asList(string.split(delimiter));
+    static int add(String numbers, String deli) {
 
+        String[] arr = numbers.split("[" + deli + "]");
+        StringBuilder negativeNumbers = new StringBuilder(); //Tracking negative numbers to be displayed later
+        int sum = 0;
         try {
-            if (getIntStream(num).anyMatch(n -> n < 0))
-                throw new IllegalArgumentException();
+            for (String numberAtIndex : arr) {
+
+                if (!numberAtIndex.trim().isEmpty() && (Character.isDigit(numberAtIndex.charAt(0)) || numberAtIndex.charAt(0) == '-')) {
+
+                    if (!Character.isDigit(numbers.charAt(numbers.length() - 1)))
+                        throw new IllegalArgumentException("ERROR: invalid input");
+
+                    if (Integer.parseInt(numberAtIndex.trim()) < 0) {
+
+                        negativeNumbers.append(numberAtIndex.trim()).append(", ");
+                    } else if (Integer.parseInt(numberAtIndex.trim()) < 1000) {  //ignores integers greater than or equal to 1000
+                        sum += Integer.parseInt(numberAtIndex.trim());
+                    }
+                }
+            }
         } catch (IllegalArgumentException e) {
-            System.out.println("ERROR: negatives not allowed");
+            throw new IllegalArgumentException("ERROR: invalid input");
         }
-
-        return getIntStream(num).sum();
+        if (negativeNumbers.length() > 0) // throw going to be handled by assertThrows
+            throw new IllegalArgumentException("\nERROR: negatives not allowed " + negativeNumbers.toString().substring(0, negativeNumbers.length() - 2));
+        return sum;
     }
 
-    private static IntStream getIntStream(List<String> num) {
-        return num.stream().mapToInt(Integer::parseInt).map(n->n%1000);
-    }
-
-    public void inputs() {
-        System.out.println(add(""));
-        System.out.println(add("1"));
-        System.out.println(add("1,1"));
-        System.out.println(add("1,2,3,4"));
-        System.out.println(add("1\n2,3"));
-//        System.out.println(add("//;\\n1;2"));
-//        System.out.println(add("//4\\n142"));
-    //    System.out.println(add("-1,-2,3,4"));
-//        System.out.println(add("//;\n1000,1;2"));
-//        System.out.println(add("//***\n1***2***3"));
+    public void inputs(){
+        String[] split = "1,2|3.4$5".split("[,|.]");
+        System.out.println(Arrays.toString(split));
+        System.out.println ("add(\"\") = " +(add("")));
+        System.out.println ("add(\"2,3\") = " +(add("2,3")));
+        System.out.println ("add(\"1\\n2,3\") = " +(add("1\n2,3")));
+        System.out.println ("add(\"//;\\n1;2\") = " +(add("//;\n1;2")));
+        System.out.println ("add(\"//4\\n142\") = " +(add("//4\n142")));
+        System.out.println(add("1,-1"));
+        System.out.println ("add(\"//[:D][&]\\n:D2&3\") = " +(add("//[:D][&]\n:D2&3")));
+        System.out.println("add(\"//[***]\\n1***2***3\") = "+(add("//[***]\n1***2***3")));
     }
 }
